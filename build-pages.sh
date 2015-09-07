1beaa373242ea5222497d4bc0a9964a711114d02
Joshua T Corbin <jcorbin@wunjo.org>
2015-09-06 22:56:44 -0700

    Unify html files by more sed hacks

--- build-pages.sh
+++ build-pages.sh
@@ -14,7 +14,6 @@ git merge --no-edit $branch
 
 bundle index.js >index-bundle.js
 minify index-bundle.js >index-bundle-min.js
-html-inline -i index.html -o index.html
 
 desc=$(git describe --always $branch)
 if [ $(git cat-file -t $desc) == 'tag' ]; then
@@ -24,7 +23,13 @@ else
     desc=$(git describe --always HEAD)
 fi
 
-sed -e "s/DESCRIBE/$desc/" index-inline-min.html >index.html
+sed \
+    -e "/data-import/s/src=[^>]*>/src=\"index-bundle-min.js\">/" \
+    -e "s/DEV/$desc/" \
+    -e "/PROJECT LINK/d" \
+    index-dev.html >index-tmp.html
+html-inline -i index-tmp.html -o index.html
+
 git commit -a -m 'gh-pages: update build'
 if [ -n "$tagit" ]; then
     git tag -a -m "Build $desc" $tagit HEAD
