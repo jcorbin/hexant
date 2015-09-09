e39474786ba734bf856a186f2fbb412073ab926e
Joshua T Corbin <jcorbin@wunjo.org>
2015-09-08 22:01:34 -0700

    Factor out rules module

--- hexant.js
+++ hexant.js
@@ -11,44 +11,9 @@ var Ant = require('./ant.js');
 var Hash = require('./hash.js');
 var OddQOffset = require('./coord.js').OddQOffset;
 var HexTileTree = require('./hextiletree.js');
+var rules = require('./rules.js');
 
 var BatchLimit = 256;
-
-var RulesLegend = 'W=West, L=Left, A=Ahead, R=Right, E=East, F=Flip';
-var Rules = {
-    W: -2,
-    L: -1,
-    A: 0,
-    R: 1,
-    E: 2,
-    F: 3
-};
-
-function parseRule(rule) {
-    rule = rule.toUpperCase();
-    var parts = rule.split('');
-    var rules = [];
-    for (var i = 0; i < parts.length; i++) {
-        var r = Rules[parts[i]];
-        if (r !== undefined) {
-            rules.push(r);
-        }
-    }
-    return rules;
-}
-
-function ruleToString(rules) {
-    var rule = '';
-    var ruleKeys = Object.keys(Rules);
-    for (var i = 0; i < rules.length; i++) {
-        for (var j = 0; j < ruleKeys.length; j++) {
-            if (rules[i] === Rules[ruleKeys[j]]) {
-                rule += ruleKeys[j];
-            }
-        }
-    }
-    return rule;
-}
 
 function Hexant(body, scope) {
     var self = this;
@@ -101,7 +66,7 @@ Hexant.prototype.hookup = function hookup(id, component, scope) {
     this.world.addAnt(new Ant(this.world));
 
     this.hash.bind('rule')
-        .setParse(parseRule, ruleToString)
+        .setParse(rules.parse, rules.toString)
         .setDefault('LR')
         .addListener(function onRuleChange(rules) {
             var ant = self.world.ants[0];
@@ -158,7 +123,7 @@ function onKeyPress(e) {
         break;
     case 0x2f: // /
         var rule = this.hash.getStr('rule');
-        rule = prompt('New Rules: (' + RulesLegend + ')', rule);
+        rule = prompt('New Rules: (' + rules.help + ')', rule);
         if (typeof rule === 'string') {
             this.pause();
             this.hash.set('rule', rule);
