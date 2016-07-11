fb1a4095679f752cbb276974f7d4d31fd7dbe7e9
Joshua T Corbin <jcorbin@wunjo.org>
2016-07-10 22:04:20 -0700

    Factor out core build script

--- scripts/build-pages.sh
+++ scripts/build-pages.sh
@@ -12,10 +12,6 @@ tagit=
 git checkout -f gh-pages
 git merge --no-edit $branch
 
-npm run grammar
-bundle index.js >index-bundle.js
-minify index-bundle.js >index-bundle-min.js
-
 desc=$(git describe --always $branch)
 if [ $(git cat-file -t $desc) == 'tag' ]; then
     desc="build-$desc"
@@ -24,14 +20,10 @@ else
     desc=$(git describe --always HEAD)
 fi
 
-sed \
-    -e "/data-import/s/src=[^>]*>/src=\"index-bundle-min.js\">/" \
-    -e "s/DEV/$desc/" \
-    -e "/PROJECT LINK/d" \
-    index-dev.html >index-tmp.html
-html-inline -i index-tmp.html -o index.html
+npm run build
 
 git commit -a -m 'gh-pages: update build'
+
 if [ -n "$tagit" ]; then
     git tag -a -m "Build $desc" $tagit HEAD
 fi
