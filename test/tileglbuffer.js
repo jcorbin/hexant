24962ead57f3f9eac06841ff7384d9ecd34ef537
Joshua T Corbin <jcorbin@wunjo.org>
2016-09-15 14:54:50 -0700

    Add and test tileglbuffer.collectTombstone

--- test/tileglbuffer.js
+++ test/tileglbuffer.js
@@ -3,3 +3,161 @@
 var test = require('tape');
 
 var tileglbuf = require('../tileglbuffer.js');
+
+[
+    {
+        desc: 'nothing to do, story checks out',
+        start: [
+            1, 32,
+            null, 32,
+            3, 32
+        ],
+        collect: [2, 32],
+        expected: [
+            1, 32,
+            null, 32,
+            3, 32
+        ]
+    },
+    {
+        desc: 'need to split',
+        start: [
+            1, 32,
+            null, 32,
+            3, 32
+        ],
+        collect: [2, 16],
+        expected: [
+            1, 32,
+            null, 16,
+            null, 16,
+            3, 32
+        ]
+    },
+    {
+        desc: 'can gift',
+        start: [
+            1, 32,
+            null, 32,
+            null, 32,
+            3, 32
+        ],
+        collect: [2, 16],
+        expected: [
+            1, 32,
+            null, 16,
+            null, 48,
+            3, 32
+        ]
+    },
+    {
+        desc: 'coalesce-2, but then it works out',
+        start: [
+            1, 32,
+            null, 32,
+            null, 32,
+            4, 32
+        ],
+        collect: [2, 64],
+        expected: [
+            1, 32,
+            null, 64,
+            4, 32
+        ]
+    },
+    {
+        desc: 'coalesce-2 and split',
+        start: [
+            1, 32,
+            null, 32,
+            null, 48,
+            4, 32
+        ],
+        collect: [2, 64],
+        expected: [
+            1, 32,
+            null, 64,
+            null, 16,
+            4, 32
+        ]
+    },
+    {
+        desc: 'coalesce-2 and gift',
+        start: [
+            1, 32,
+            null, 32,
+            null, 48,
+            null, 8,
+            4, 32
+        ],
+        collect: [2, 64],
+        expected: [
+            1, 32,
+            null, 64,
+            null, 24,
+            4, 32
+        ]
+    },
+    {
+        desc: 'coalesce-3, but then it works out',
+        start: [
+            1, 32,
+            null, 32,
+            null, 32,
+            null, 32,
+            4, 32
+        ],
+        collect: [2, 96],
+        expected: [
+            1, 32,
+            null, 96,
+            4, 32
+        ]
+    },
+    {
+        desc: 'coalesce-3 and split',
+        start: [
+            1, 32,
+            null, 32,
+            null, 48,
+            null, 32,
+            4, 32
+        ],
+        collect: [2, 96],
+        expected: [
+            1, 32,
+            null, 96,
+            null, 16,
+            4, 32
+        ]
+    },
+    {
+        desc: 'coalesce-3 and gift',
+        start: [
+            1, 32,
+            null, 32,
+            null, 48,
+            null, 32,
+            null, 8,
+            4, 32
+        ],
+        collect: [2, 96],
+        expected: [
+            1, 32,
+            null, 96,
+            null, 24,
+            4, 32
+        ]
+    }
+].forEach(function testEach(testCase) {
+    test('collectTombstone: ' + testCase.desc, function t(assert) {
+        var tiles = testCase.start.slice(0);
+        for (var i = 0; i < testCase.collect.length; i+=2) {
+            var j = testCase.collect[i];
+            var n = testCase.collect[i+1];
+            tileglbuf.collectTombstone(tiles, j, n);
+        }
+        assert.deepEqual(tiles, testCase.expected, JSON.stringify(testCase.expected));
+        assert.end();
+    });
+});
--- tileglbuffer.js
+++ tileglbuffer.js
@@ -1,3 +1,55 @@
 'use strict';
 
 // TODO: take over more code, currently this module only has code to be tested
+
+function collectTombstone(tiles, i, length) {
+    if (tiles[i] !== null) {
+        throw new Error('not a tombstone');
+    }
+
+    var tileLength = tiles[i+1];
+    tiles[i+1] = length;
+
+    // coalesce range; we assume that we've been told an index of a usable set
+    // of tombstones, and so don't range check here
+    var j = i + 2;
+    var spare = 0;
+    for (; tileLength < length; j += 2) {
+        tileLength += tiles[j+1];
+        spare += 2;
+    }
+
+    // truncate (finish any coalesce)
+    if (spare > 0) {
+        var k = i + 2;
+        while (j < tiles.length) {
+            tiles[k++] = tiles[j++];
+        }
+        j = i + 2;
+    }
+
+    // distribute leftover
+    if (length < tileLength) {
+        var remain = tileLength - length;
+        if (tiles[j] === null) {
+            // easy, give it to the next tombstone
+            tiles[j+1] += remain;
+        } else {
+            // split into new tombstone
+            var n = tiles.length - j;
+            if (spare >= 2) {
+                spare -= 2;
+            } else {
+                tiles.push(0, 0);
+            }
+            var k = tiles.length - 1;
+            for (; n-- > 0; k--) tiles[k] = tiles[k - 2];
+            tiles[j] = null;
+            tiles[j+1] = remain;
+        }
+    }
+
+    tiles.length -= spare;
+}
+
+module.exports.collectTombstone = collectTombstone;
