5a85389985fc251afb2a0e25584e35205f3dde87
Joshua T Corbin <jcorbin@wunjo.org>
2022-04-19 15:53:12 -0400

    Uplift glpalette.js

--- src/glpalette.js
+++ src/glpalette.js
@@ -1,48 +1,95 @@
+// @ts-check
+
 'use strict';
 
-module.exports = GLPalette;
+/** @typedef {import("./colorgen.js").ColorTuple} ColorTuple */
 
-function GLPalette(gl, unit, srgb, colors) {
+export class GLPalette {
+
+  /**
+   * @param {WebGLRenderingContext} gl
+   * @param {object} options
+   * @param {number} options.unit
+   * @param {"rgb"|"srgb"} [options.format]
+   * @param {Iterable<ColorTuple>} [options.colors]
+   */
+  constructor(gl, { unit, format: formatArg = 'rgb', colors }) {
+    const format = function() {
+      if (formatArg == 'srgb') {
+        const extSRGB = gl.getExtension('EXT_sRGB');
+        if (extSRGB) {
+          return extSRGB.SRGB_EXT;
+        } else {
+          console.warn('sRGB Gl extension not available, falling back to RGB colorspace')
+        }
+      }
+      return gl.RGB;
+    }();
+    const data = new Uint8Array(256 * 3);
+    const tex = gl.createTexture();
+
     this.gl = gl;
     this.unit = unit;
-    this.extSRGB = this.gl.getExtension('EXT_sRGB');
-    this.format = srgb && this.extSRGB ? this.extSRGB.SRGB_EXT : this.gl.RGB;
-    this.data = new Uint8Array(256 * 3);
-    this.tex = this.gl.createTexture();
+    this.format = format;
+    this.data = data;
+    this.tex = tex;
 
-    this.gl.activeTexture(this.gl.TEXTURE0 + this.unit);
-    this.gl.bindTexture(this.gl.TEXTURE_2D, this.tex);
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST);
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
+    gl.activeTexture(gl.TEXTURE0 + unit);
+    gl.bindTexture(gl.TEXTURE_2D, tex);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
-    if (Array.isArray(colors)) {
-        this.setColorsRGB(colors);
+    if (colors) {
+      this.setColorsRGB(colors);
     }
+  }
+
+  /** @param {WebGLUniformLocation} uSampler */
+  use(uSampler) {
+    const { gl, unit } = this;
+    gl.activeTexture(gl.TEXTURE0 + unit);
+    gl.uniform1i(uSampler, unit);
+  }
+
+  /** @param {Iterable<ColorTuple>} [rgbColors] */
+  setColorsRGB(rgbColors = []) {
+    const { gl, unit, tex, format, data } = this;
+    data.fill(0);
+    setNumbers(data, function*() {
+      for (const [r, g, b] of rgbColors) {
+        yield Math.round(255 * r);
+        yield Math.round(255 * g);
+        yield Math.round(255 * b);
+      }
+    }());
+
+    gl.activeTexture(gl.TEXTURE0 + unit);
+    gl.bindTexture(gl.TEXTURE_2D, tex);
+    gl.texImage2D(
+      gl.TEXTURE_2D, 0, format,
+      256, 1, 0,
+      format, gl.UNSIGNED_BYTE, data);
+  }
+
 }
 
-GLPalette.prototype.use =
-function use(uSampler) {
-    this.gl.activeTexture(this.gl.TEXTURE0 + this.unit);
-    this.gl.uniform1i(uSampler, this.unit);
-};
+// TODO merge with view_gl.js
 
-GLPalette.prototype.setColorsRGB =
-function setColorsRGB(colors) {
-    for (var i = 0, j = 0; i < colors.length; ++i) {
-        var color = colors[i];
-        this.data[j++] = Math.round(255 * color[0]);
-        this.data[j++] = Math.round(255 * color[1]);
-        this.data[j++] = Math.round(255 * color[2]);
-    }
-    while (j < this.data.length) {
-        this.data[j++] = 0;
-    }
-    this.gl.activeTexture(this.gl.TEXTURE0 + this.unit);
-    this.gl.bindTexture(this.gl.TEXTURE_2D, this.tex);
-    this.gl.texImage2D(
-        this.gl.TEXTURE_2D, 0, this.format,
-        256, 1, 0,
-        this.format, this.gl.UNSIGNED_BYTE, this.data);
-};
+/**
+ * @param {{readonly length: number, [index: number]: number}|null} ar
+ * @param {Iterable<number>} ns
+ */
+function setNumbers(ar, ns) {
+  const length = ar?.length;
+  if (!length) {
+    return;
+  }
+  let i = 0;
+  for (const n of ns) {
+    ar[i] = n;
+    if (++i >= length) { return; }
+  }
+}
+
