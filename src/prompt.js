8bbea2678868a1aa9785524db369db922e193b18
Joshua T Corbin <jcorbin@wunjo.org>
2022-04-20 13:57:46 -0400

    Uplift prompt.js
    Take a local stab at managing binding elements

--- src/prompt.js
+++ src/prompt.js
@@ -1,121 +1,217 @@
+// @ts-check
+
 'use strict';
 
-export class Prompt{
+/** @typedef {Canceled|Response} Result */
+/** @typedef {{canceled: true, value?: undefined}} Canceled */
+/** @typedef {{value: string, canceled?: false}} Response */
 
+export class Prompt {
+
+  /**
+   * @param {object} params
+   * @param {HTMLElement} params.$body
+   * @param {HTMLElement} [params.$help]
+   * @param {HTMLTextAreaElement} [params.$text]
+   * @param {HTMLElement} [params.$error]
+   */
   constructor({
-    body,
-    help=body.querySelecotr('#help'),
-    text=body.querySelecotr('#text'),
-    error=body.querySelecotr('#error'),
+    $body,
+    $help = mustQuery($body, '#help', HTMLElement),
+    $text = mustQuery($body, '#text', HTMLTextAreaElement),
+    $error = mustQuery($body, '#error', HTMLElement),
   }) {
-    this.box = body;
-    this.help = help;
-    this.error = error;
-    this.text = text;
+    this.$box = $body;
+    this.$help = $help;
+    this.$text = $text;
+    this.$error = $error;
+
+    /** @type {((res: Result) => void)|null} */
     this.callback = null;
     this.lastEnter = 0;
 
-    this.text.addEventListener('keydown', e => this.onKeyDown(e));
-    this.text.addEventListener('keyup', e => this.onKeyUp(e));
-    this.text.addEventListener('blur', () => this.cancel());
+    this.$text.addEventListener('keydown', this);
+    this.$text.addEventListener('keyup', this);
+    this.$text.addEventListener('blur', this);
   }
 
   active() {
     return !!this.callback;
   }
 
-  prompt(help, value, callback) {
-    this.help.innerText = help;
-    this.text.value = value;
-    this.callback = callback;
-    this.box.style.display = '';
+  /**
+   * @param {string} help
+   * @param {string} value
+   * @returns {Generator<Promise<Result>>}
+   */
+  *interact(help, value) {
+    const restore = this.stash();
+    try {
+      this.prompt(help, value);
+      while (this.$box.style.display != 'none') {
+        yield new Promise(resolve => this.callback = resolve);
+      }
+    } finally {
+      restore();
+    }
+  }
+
+  /**
+   * @param {string} help
+   * @param {string} value
+   */
+  prompt(help, value) {
+    // TODO refactor to return a promise, rather than take a callback
+    this.$help.innerText = help;
+    this.$text.value = value;
+    this.$box.style.display = '';
     this.resizeTextRows();
-    this.text.select();
-    this.text.focus();
+    this.$text.select();
+    this.$text.focus();
+  }
+
+  stash() {
+    const {
+      $box: { style: { display } },
+      $help: { innerText: help },
+      $text: { value },
+      $error: {
+        innerText: err,
+        style: { display: errDisplay },
+      },
+      callback,
+      lastEnter,
+    } = this;
+    return () => {
+      this.$box.style.display = display;
+      this.$help.innerText = help;
+      this.$text.value = value;
+      this.$error.innerText = err;
+      this.$error.style.display = errDisplay;
+      this.callback = callback;
+      this.lastEnter = lastEnter;
+    };
   }
 
   resizeTextRows() {
-    const lines = this.text.value.split(/\n/);
-    this.text.rows = lines.length + 1;
+    const lines = this.$text.value.split(/\n/);
+    this.$text.rows = lines.length + 1;
   }
 
-  finish() {
-    let value = this.text.value;
+  respond() {
+    let value = this.$text.value;
     const callback = this.callback;
-
     if (callback) {
       value = value.replace(/(?:\r?\n)+$/, '');
-      callback(false, value, (err, help, revalue) => {
-        this.finished(err, help, revalue);
-      });
+      callback({ value });
     } else {
-      this.finished(null, '', '');
+      this.hide();
     }
   }
 
   cancel() {
     const callback = this.callback;
     if (callback) {
-      callback(true, null, (err, help, revalue) => {
-        this.finished(err, help, revalue);
-      });
+      callback({ canceled: true });
+    } else {
+      this.hide();
     }
   }
 
-  finished(err, help, revalue) {
-    if (err) {
-      this.error.innerText = '' + err;
-      this.error.style.display = '';
-      if (help) {
-        this.help.innerText = help;
-      }
-      if (revalue) {
-        this.text.value = revalue;
-      }
-      return;
+  /**
+   * @param {string} err
+   * @param {string} [help]
+   * @param {string} [revalue]
+   */
+  error(err, help, revalue) {
+    this.$error.innerText = err;
+    this.$error.style.display = '';
+    if (help) {
+      this.$help.innerText = help;
     }
-    this.hide();
+    if (revalue) {
+      this.$text.value = revalue;
+    }
   }
 
   hide() {
+    this.$box.style.display = 'none';
+    this.$help.innerText = '';
+    this.$text.value = '';
+    this.$error.style.display = 'none';
+    this.$error.innerText = '';
     this.lastEnter = 0;
-    this.box.style.display = 'none';
     this.callback = null;
-    this.text.value = '';
-    this.help.innerText = '';
-    this.error.style.display = 'none';
-    this.error.innerText = '';
   }
 
-  onKeyDown(e) {
-    switch (e.keyCode) {
-    case 0x0d: // <Enter>
-      if (e.ctrlKey) {
-        e.preventDefault();
-      }
-      break;
-    case 0x1b: // <Esc>
-      this.cancel();
-      e.preventDefault();
-      return;
+  /** @param {Event} e */
+  handleEvent(e) {
+    switch (e.type) {
+
+      case 'blur':
+        this.cancel();
+        break;
+
+      case 'keydown':
+        if (e instanceof KeyboardEvent) {
+          switch (e.key) {
+
+            case 'Enter':
+              if (e.ctrlKey) {
+                e.preventDefault();
+              }
+              break;
+
+            case 'Escape':
+              this.cancel();
+              e.preventDefault();
+              return;
+
+          }
+          this.resizeTextRows();
+        }
+        break;
+
+      case 'keyup':
+        if (e instanceof KeyboardEvent) {
+          switch (e.key) {
+
+            case 'Enter':
+              if (Date.now() - this.lastEnter < 1000 || e.ctrlKey) {
+                e.preventDefault();
+                this.respond();
+                return;
+              }
+              this.lastEnter = Date.now();
+              break;
+
+            default:
+              this.lastEnter = 0;
+
+          }
+          this.resizeTextRows();
+        }
+        break;
+
     }
-    this.resizeTextRows();
   }
 
-  onKeyUp(e) {
-    switch (e.keyCode) {
-    case 0x0d: // <Enter>
-      if (Date.now() - this.lastEnter < 1000 || e.ctrlKey) {
-        e.preventDefault();
-        this.finish();
-        return;
-      }
-      this.lastEnter = Date.now();
-      break;
-    default:
-      this.lastEnter = 0;
-    }
-    this.resizeTextRows();
-  }
+}
 
+/**
+ * @template {abstract new(...args: any[]) => any} T
+ * @param {HTMLElement} el
+ * @param {string} selector
+ * @param {T} type
+ * @returns {InstanceType<T>}
+ */
+function mustQuery(el, selector, type) {
+  const res = el.querySelector(selector);
+  if (!res) {
+    throw new Error(`unable to find a ${selector}`);
+  }
+  if (!(res instanceof type)) {
+    throw new Error(`invalid ${selector} element, must be a ${type.name} instance`);
+  }
+  return /** @type {InstanceType<T>} */ (res);
 }
