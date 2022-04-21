18331c05efea91f01f99718d26fd0b2440e0a6eb
Joshua T Corbin <jcorbin@wunjo.org>
2022-04-21 10:37:53 -0400

    Uplift world.js

--- src/world.js
+++ src/world.js
@@ -1,226 +1,292 @@
+// @ts-check
+
 'use strict';
 
-var Coord = require('./coord.js');
-var HexTileTree = require('./hextiletree.js');
-var CubePoint = Coord.CubePoint;
+import { CubePoint, OddQOffset } from './coord.js';
+import { HexTileTree } from './hextiletree.js';
 
-var REDRAW_TIMING_WINDOW = 5000;
+const REDRAW_TIMING_WINDOW = 5000;
 
-module.exports = World;
+// TODO refactor ent to be system oriented, pushing array ownership out of
+// World into a passed in EntSystem interface
 
-World.StateShift      = 8;
-World.ColorShift      = 8;
-World.TurnShift       = 16;
-World.FlagVisited     = 0x0100;
-World.MaskFlags       = 0xff00;
-World.MaskColor       = 0x00ff;
-World.MaxState        = 0xff;
-World.MaxColor        = 0xff;
-World.MaxTurn         = 0xffff;
-World.MaskResultState = 0xff000000;
-World.MaskResultColor = 0x00ff0000;
-World.MaskResultTurn  = 0x0000ffff;
+/** @typedef {object} Ent
+ * @prop {number} index
+ * @prop {number} numColors
+ * @prop {number} numStates
+ * @prop {OddQOffset} pos
+ * @prop {number} dir
+ * @prop {number} state
+ * @prop {() => void} reset
+ * @prop {(world: World) => void} step
+ */
 
-function World() {
+/** @typedef {object} View
+ * @prop {() => void} step - TODO why are view stepped?
+ * @prop {() => void} reset
+ * @prop {boolean} needsRedraw -- TODO push this concern down into view?
+ * @prop {() => void} redraw
+ * @prop {(i: number) => void} addEnt
+ * @prop {(i: number) => void} updateEnt
+ * @prop {(i: number) => void} removeEnt
+ * @prop {() => void} updateEnts
+ */
+
+// TODO view per-ent concerns may be better to push down into each view
+// entirely; pass EntSystem to each view?
+
+export class World {
+
+  static MaxColor = 0xff
+  static MaxState = 0xff
+  static MaxTurn = 0xffff
+
+  static MaskResultColor = 0xff000000
+  static MaskResultState = 0x00ff0000
+  static MaskResultTurn = 0x0000ffff
+
+  static ColorShift = 8
+  static TurnShift = 16
+
+  static FlagVisited = 0x0100
+  static MaskColor = 0x00ff
+  static MaskFlags = 0xff00
+
+  constructor() {
     this.numColors = 0;
     this.numStates = 0;
     this.stepCount = 0;
     this.tile = new HexTileTree();
+
+    /** @type {Ent[]} */
     this.ents = [];
+
+    /** @type {View[]} */
     this.views = [];
+
+    /** @type {number[]} */
     this.redrawTiming = [];
-}
+    this.now = () => performance.now();
 
-World.prototype.getEntPos =
-function getEntPos(i) {
-    // TODO: take ownership of these
-    return this.ents[i].pos;
-};
+    this.tmpCP = new CubePoint();
+  }
 
-World.prototype.getEntDir =
-function getEntDir(i) {
-    // TODO: take ownership of these
-    return this.ents[i].dir;
-};
+  /** @param {number} i */
+  getEntPos(i) {
+    // TODO push this method out into EntSystem implementations
+    const ent = this.ents[i];
+    return ent ? ent.pos : new OddQOffset(NaN, NaN);
+  }
 
-World.prototype.reset =
-function reset() {
-    // TODO: should carry initial pos/dir
-    if (this.ents.length > 0) {
-        this.ents[0].reset();
-        this.ents[0].pos.scale(0); // reset to 0,0
-        this.ents[0].dir = 0;
+  /** @param {number} i */
+  getEntDir(i) {
+    // TODO push this method out into EntSystem implementations
+    const ent = this.ents[i];
+    return ent ? ent.dir : NaN;
+  }
+
+  reset() {
+    const { ents, tile, views } = this;
+
+    // TODO push out into EntSystem.reset()
+    for (const ent of ents) {
+      ent.reset();
     }
-    this.tile.reset();
+
+    tile.reset();
     this.stepCount = 0;
-    var i = 0;
-    for (; i < this.views.length; ++i) {
-        this.views[i].reset();
-    }
-    for (i = 0; i < this.ents.length; ++i) {
-        this.tile.update(this.getEntPos(i), markVisited);
-    }
-};
 
-World.prototype.turnEnt =
-function turnEnt(i, turnFunc) {
-    var dir = turnFunc(this.ents[i].dir);
-    this.ents[i].dir = dir;
-    this.tile.update(
-        this.ents[i].pos.add(CubePoint.basis[dir]),
-        markVisited);
-};
+    for (const view of views) {
+      view.reset();
+    }
 
-World.prototype.step =
-function step() {
-    var i;
-    for (i = 0; i < this.ents.length; i++) {
-        this.ents[i].step(this);
+    // TODO push out into EntSystem.occupiedPositions()
+    const occupiedPositions = ents.map(({ pos }) => pos);
+    for (const pos of occupiedPositions) {
+      tile.update(pos, datum => World.FlagVisited | datum);
     }
-    for (i = 0; i < this.views.length; i++) {
-        this.views[i].step();
-    }
-    this.stepCount++;
+
+  }
+
+  /**
+   * @param {number} i
+   * @param {(dir: number, state: number, datum: number) => {dir: number, state: number, datum: number}} fn
+   */
+  updateEnt(i, fn) {
+    // TODO refactor into unified EntSystem.step(tile);
+    //      currently this is called by Turmite.prototype.step() ala Ent.step()
+
+    const { ents, tile, tmpCP } = this;
+    const ent = ents[i];
+    if (!ent) { return }
+    const { pos, dir, state } = ent;
+    tile.update(pos, datum => {
+      const { dir: newDir, state: newState, datum: newDatum } = fn(dir, state, datum);
+      const basis = CubePoint.basis[newDir];
+      if (basis) {
+        pos.toCubeInto(tmpCP)
+          .add(basis)
+          .toOddQOffsetInto(pos);
+      }
+      ent.dir = newDir;
+      ent.state = newState;
+      return newDatum;
+    });
+    tile.update(ent.pos, datum => World.FlagVisited | datum);
+  }
+
+  step() {
+    this._step();
     this.redraw();
-};
+  }
 
-World.prototype.stepn =
-function stepn(n) {
-    for (var i = 0; i < n; i++) {
-        var j;
-        for (j = 0; j < this.ents.length; j++) {
-            this.ents[j].step(this);
-        }
-        for (j = 0; j < this.views.length; j++) {
-            this.views[j].step();
-        }
+  /** @param {number} n */
+  stepn(n) {
+    for (let i = 0; i < n; i++) {
+      this._step();
     }
     this.stepCount += n;
     return this.redraw();
-};
+  }
 
-World.prototype.redraw =
-function redraw() {
-    var didredraw = false;
-    var t0 = Date.now();
-    for (var i = 0; i < this.views.length; i++) {
-        var view = this.views[i];
-        if (view.needsRedraw) {
-            view.redraw();
-            didredraw = true;
-        }
+  _step() {
+    // TODO becomes EntSystem.step()
+    for (const ent of this.ents) {
+      ent.step(this);
     }
-    var t1 = Date.now();
+    // TODO why are view stepped? that sounds like a (re)drawing concern?
+    for (const view of this.views) {
+      view.step();
+    }
+    this.stepCount++;
+  }
+
+  redraw() {
+    let didredraw = false;
+
+    const t0 = this.now();
+    for (const view of this.views) {
+      if (view.needsRedraw) {
+        view.redraw();
+        didredraw = true;
+      }
+    }
+    const t1 = this.now();
+
+    // TODO encapulate similarly to Sample... TimingSample?
     if (didredraw) {
-        while (t0 - this.redrawTiming[0] > REDRAW_TIMING_WINDOW) {
-            this.redrawTiming.shift();
-            this.redrawTiming.shift();
-        }
-        this.redrawTiming.push(t0, t1);
+      const { redrawTiming } = this;
+      while (
+        redrawTiming[0] !== undefined &&
+        t0 - redrawTiming[0] > REDRAW_TIMING_WINDOW
+      ) {
+        redrawTiming.shift();
+        redrawTiming.shift();
+      }
+      redrawTiming.push(t0, t1);
     }
+
     return didredraw;
-};
+  }
 
-World.prototype.redrawTimingStats =
-function redrawTimingStats() {
-    var i = 0, n = 0, m1 = 0, m2 = 0;
-    while (i < this.redrawTiming.length) {
-        var t0 = this.redrawTiming[i++];
-        var t1 = this.redrawTiming[i++];
-        var dur = t1 - t0;
-        var delta = dur - m1;
-        m1 += delta / ++n;
-        m2 += delta * (dur - m1);
+  redrawTimingStats() {
+    const { redrawTiming } = this;
+    if (redrawTiming.length < 4) {
+      return null;
     }
-    if (n < 2) {
-        return null;
+
+    let n = 0, m1 = 0, m2 = 0;
+
+    let i = 0;
+    while (i < redrawTiming.length) {
+      // NOTE: the ||NaN is mainly for undefined proof, but 0 is also not a
+      // reasonable number in redrawTiming, so annihilating any maths circa a
+      // spurious 0 is likely a Good Idea ™️ ... or is at least more likely to
+      // get noticed sooner ;-)
+      const t0 = redrawTiming[i++] || NaN;
+      const t1 = redrawTiming[i++] || NaN;
+      const dur = t1 - t0;
+      const delta = dur - m1;
+      n++
+      m1 += delta / n;
+      m2 += delta * delta;
     }
+
     m2 /= n - 1;
-    return {
-        n: n,
-        m1: m1,
-        m2: m2
-    };
-};
+    return { n, m1, m2 };
+  }
 
-World.prototype.removeEnt =
-function removeEnt(ent) {
-    if (this.ents[ent.index] !== ent) {
-        throw new Error('removeEnt mismatch');
-    }
-    this._removeEnt(ent.index);
-    return ent;
-};
-
-World.prototype._removeEnt =
-function _removeEnt(i) {
-    var j = i++;
-    for (; j < this.ents.length; i++, j++) {
-        this.ents[i] = this.ents[j];
-        this.ents[i].index = i;
-    }
-    this.ents.pop();
-
-    for (i = 0; i < this.views.length; i++) {
-        this.views[i].removeEnt(i);
-    }
-};
-
-World.prototype.setEnts =
-function setEnts(ents) {
-    var cons = ents[0].constructor;
-    var i;
-    var j;
-    for (i = 1; i < ents.length; ++i) {
-        if (ents[i].constructor !== cons) {
-            throw new Error('setEnts must get a list of same-type ents');
-        }
+  /** @param {Ent} ent */
+  removeEnt(ent) {
+    // TODO push out into EntSystem internal concerns;
+    //      does not need to be on the World : EntSystem surface
+    const { index } = ent;
+    const { ents, views } = this;
+    if (ents[index] !== ent) {
+      throw new Error('removeEnt mismatch');
     }
 
-    if (ents.length < this.ents.length) {
-        for (i = ents.length; i < this.ents.length; i++) {
-            for (j = 0; j < this.views.length; j++) {
-                this.views[j].removeEnt(i);
-            }
-        }
-        this.ents.length = ents.length;
-    }
-
-    var n = this.ents.length;
-    for (i = 0; i < ents.length; i++) {
-        var ent = ents[i];
+    let i = index;
+    for (let j = i++; j < ents.length; j++) {
+      const ent = ents[j];
+      if (ent) {
+        ents[i] = ent;
         ent.index = i;
-        this.ents[i] = ent;
-        this.tile.update(ent.pos, markVisited);
+        i++;
+      }
+    }
+    ents.length = i;
+
+    for (let view of views) {
+      view.removeEnt(i);
+    }
+  }
+
+  /** @param {Ent[]} newEnts */
+  setEnts(newEnts) {
+    const { ents, tile, views } = this;
+    const priorEntsLength = ents.length;
+
+    ents.length = 0;
+    for (const ent of newEnts) {
+      ent.index = ents.length;
+      ents.push(ent);
     }
 
-    this.numColors = 0;
-    this.numStates = 0;
-    for (j = 0; j < this.ents.length; j++) {
-        this.numColors = Math.max(this.numColors, this.ents[j].numColors);
-        this.numStates = Math.max(this.numStates, this.ents[j].numStates);
+    // TODO EntSystem.occupiedPositions()
+    const occupiedPositions = newEnts.map(({ pos }) => pos);
+    for (const pos of occupiedPositions) {
+      tile.update(pos, datum => World.FlagVisited | datum);
     }
 
-    for (i = 0; i < n; ++i) {
-        for (j = 0; j < this.views.length; j++) {
-            this.views[j].updateEnt(j);
-        }
-    }
-    for (; i < ents.length; ++i) {
-        for (j = 0; j < this.views.length; j++) {
-            this.views[j].addEnt(j);
-        }
-    }
+    // TODO EntSystem.numColors()
+    this.numColors = Math.max.apply(null, ents.map(({ numColors }) => numColors));
+    this.numStates = Math.max.apply(null, ents.map(({ numStates }) => numStates));
 
-};
+    // TODO why can't this just be for view of views view.udpateEnts()
+    let i = 0;
+    for (; i < priorEntsLength; ++i) {
+      for (const view of views) {
+        view.updateEnt(i);
+      }
+    }
+    for (; i < newEnts.length; ++i) {
+      for (const view of views) {
+        view.addEnt(i);
+      }
+    }
+    for (; i < priorEntsLength; ++i) {
+      for (const view of views) {
+        view.removeEnt(i);
+      }
+    }
+  }
 
-World.prototype.addView =
-function addView(view) {
+  /** @param {View} view */
+  addView(view) {
     this.views.push(view);
-    view.updateEnts();
+    view.updateEnts(); // 🤦 and yet we had to do all that update/add/remove per-ent dance above?🍐wut
     return view;
-};
+  }
 
-function markVisited(data) {
-    return World.FlagVisited | data;
 }
