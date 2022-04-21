a34790c054232cfde973592a3e6902c87f5d3deb
Joshua T Corbin <jcorbin@wunjo.org>
2022-04-21 19:03:13 -0400

    Uplift turmite/constants.js

--- src/turmite/constants.js
+++ src/turmite/constants.js
@@ -1,79 +1,86 @@
+// @ts-check
+
 'use strict';
 
-/* relative turns
- *    F -- +0 -- no turn, forward
- *    B -- +3 -- u turn, backaward
- *    P -- -2 -- double left turn
- *    L -- -1 -- left turn
- *    R -- +1 -- right turn
- *    S -- +2 -- double right turn
- *
- * absolute turns (for "flat-top" (odd or even q)
- *   NW -- ? -- North West
- *   NO -- ? -- North
- *   NE -- ? -- North East
- *   SE -- ? -- South East
- *   SO -- ? -- South
- *   SW -- ? -- South West
- */
+/** @enum {number} */
+export const Turn = {
+  // relative turns
+  //    F -- +0 -- no turn, forward
+  //    B -- +3 -- u turn, backaward
+  //    P -- -2 -- double left turn
+  //    L -- -1 -- left turn
+  //    R -- +1 -- right turn
+  //    S -- +2 -- double right turn
+  RelForward: 0x0001,
+  RelBackward: 0x0002,
+  RelLeft: 0x0004,
+  RelRight: 0x0008,
+  RelDoubleLeft: 0x0010,
+  RelDoubleRight: 0x0020,
 
-var Turn            = {};
-Turn.RelForward     = 0x0001;
-Turn.RelBackward    = 0x0002;
-Turn.RelLeft        = 0x0004;
-Turn.RelRight       = 0x0008;
-Turn.RelDoubleLeft  = 0x0010;
-Turn.RelDoubleRight = 0x0020;
-Turn.AbsNorthWest   = 0x0040;
-Turn.AbsNorth       = 0x0080;
-Turn.AbsNorthEast   = 0x0100;
-Turn.AbsSouthEast   = 0x0200;
-Turn.AbsSouth       = 0x0400;
-Turn.AbsSouthWest   = 0x0800;
+  // absolute turns (for "flat-top" odd or even q hexagons)
+  //   NW -- ? -- North West
+  //   NO -- ? -- North
+  //   NE -- ? -- North East
+  //   SE -- ? -- South East
+  //   SO -- ? -- South
+  //   SW -- ? -- South West
+  AbsNorthWest: 0x0040,
+  AbsNorth: 0x0080,
+  AbsNorthEast: 0x0100,
+  AbsSouthEast: 0x0200,
+  AbsSouth: 0x0400,
+  AbsSouthWest: 0x0800,
+};
 
-var RelTurnDelta                  = [];
-RelTurnDelta[Turn.RelBackward]    =  3;
-RelTurnDelta[Turn.RelDoubleLeft]  = -2;
-RelTurnDelta[Turn.RelLeft]        = -1;
-RelTurnDelta[Turn.RelForward]     =  0;
-RelTurnDelta[Turn.RelRight]       =  1;
-RelTurnDelta[Turn.RelDoubleRight] =  2;
+/** @type {Map<Turn, number>} */
+export const RelTurnDelta = new Map([
+  [Turn.RelBackward, 3],
+  [Turn.RelDoubleLeft, -2],
+  [Turn.RelLeft, -1],
+  [Turn.RelForward, 0],
+  [Turn.RelRight, 1],
+  [Turn.RelDoubleRight, 2],
+]);
 
-var AbsTurnDir                = [];
-AbsTurnDir[Turn.AbsSouthEast] = 0;
-AbsTurnDir[Turn.AbsSouth]     = 1;
-AbsTurnDir[Turn.AbsSouthWest] = 2;
-AbsTurnDir[Turn.AbsNorthWest] = 3;
-AbsTurnDir[Turn.AbsNorth]     = 4;
-AbsTurnDir[Turn.AbsNorthEast] = 5;
+/** @type {Map<Turn, number>} */
+export const AbsTurnDir = new Map([
+  [Turn.AbsSouthEast, 0],
+  [Turn.AbsSouth, 1],
+  [Turn.AbsSouthWest, 2],
+  [Turn.AbsNorthWest, 3],
+  [Turn.AbsNorth, 4],
+  [Turn.AbsNorthEast, 5],
+]);
 
-var RelTurnSymbols                  = [];
-RelTurnSymbols[Turn.RelBackward]    = 'B';
-RelTurnSymbols[Turn.RelDoubleLeft]  = 'BL';
-RelTurnSymbols[Turn.RelLeft]        = 'L';
-RelTurnSymbols[Turn.RelForward]     = 'F';
-RelTurnSymbols[Turn.RelRight]       = 'R';
-RelTurnSymbols[Turn.RelDoubleRight] = 'BR';
+/** @type {Map<Turn, string>} */
+export const RelTurnSymbol = new Map([
+  [Turn.RelBackward, 'B'],
+  [Turn.RelDoubleLeft, 'BL'],
+  [Turn.RelLeft, 'L'],
+  [Turn.RelForward, 'F'],
+  [Turn.RelRight, 'R'],
+  [Turn.RelDoubleRight, 'BR'],
+]);
 
-var RelSymbolTurns = {};
-RelSymbolTurns.B   = Turn.RelBackward;
-RelSymbolTurns.P   = Turn.RelDoubleLeft;
-RelSymbolTurns.L   = Turn.RelLeft;
-RelSymbolTurns.F   = Turn.RelForward;
-RelSymbolTurns.R   = Turn.RelRight;
-RelSymbolTurns.S   = Turn.RelDoubleRight;
+// TODO why no AbsTurnSymbol?
 
-var AbsSymbolTurns = {};
-AbsSymbolTurns.NW  = Turn.AbsNorthWest;
-AbsSymbolTurns.NO  = Turn.AbsNorth;
-AbsSymbolTurns.NE  = Turn.AbsNorthEast;
-AbsSymbolTurns.SE  = Turn.AbsSouthEast;
-AbsSymbolTurns.SO  = Turn.AbsSouth;
-AbsSymbolTurns.SW  = Turn.AbsSouthWest;
+/** @type {Map<string, Turn>} */
+export const RelSymbolTurns = new Map([
+  ['B', Turn.RelBackward],
+  ['P', Turn.RelDoubleLeft],
+  ['L', Turn.RelLeft],
+  ['F', Turn.RelForward],
+  ['R', Turn.RelRight],
+  ['S', Turn.RelDoubleRight],
+]);
 
-module.exports.Turn           = Turn;
-module.exports.RelTurnDelta   = RelTurnDelta;
-module.exports.AbsTurnDir     = AbsTurnDir;
-module.exports.RelTurnSymbols = RelTurnSymbols;
-module.exports.RelSymbolTurns = RelSymbolTurns;
-module.exports.AbsSymbolTurns = AbsSymbolTurns;
+/** @type {Map<string, Turn>} */
+export const AbsSymbolTurns = new Map([
+  ['NW', Turn.AbsNorthWest],
+  ['NO', Turn.AbsNorth],
+  ['NE', Turn.AbsNorthEast],
+  ['SE', Turn.AbsSouthEast],
+  ['SO', Turn.AbsSouth],
+  ['SW', Turn.AbsSouthWest],
+]);
