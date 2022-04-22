54e297ae4148edde2aca099956ec35c535f4cd17
Joshua T Corbin <jcorbin@wunjo.org>
2022-04-22 09:58:12 -0400

    Uplift turmite/rle-builder.js
    While the old `consume`/`finish` surface exists, callers should try to
    use the new `from` iterator surface.

--- src/turmite/rle-builder.js
+++ src/turmite/rle-builder.js
@@ -1,42 +1,89 @@
+// @ts-check
+
 'use strict';
 
-module.exports = RLEBuilder;
+/** Builds a canonical RLE string from possibly redundant parts.
+ *
+ * Exemplars:
+ * - "A A A" => "3A"
+ * - "3A 2A" => "5A"
+ *
+ * Except that all tokenization concerns are up to the caller, so this builder
+ * takes pre-parsed (count: number, sym: string) pairs, and returns a list
+ * containing accumulated terms (not including the current one). The caller
+ * shouldn't really care about the return value until after a terminal () call.
+ *
+ * Usage:
+ *   const input = 'A B B 3C 3D D';
+ *   const {consume, finish} = RLEBuilder();
+ *   for (const token of input.split(/\s+/)) {
+ *     consume(token);
+ *     // NOTE: caller may parser their own count/sym apart if necessary and call consume(count, sym)
+ *   }
+ *   const output = finish().join(' ');
+ *   // output = "A 2B 3C 4D"
+ *
+ * NOTE: any empty symbol strings and counts <= 0 are elided
+ */
+export function RLEBuilder() {
+  /** @type {[count: number, sym: string][]} */
+  let parts = [];
+  let cur = '', curCount = 0;
 
-function RLEBuilder(prefix, sep, suffix) {
-    build.prefix = prefix;
-    build.sep    = sep;
-    build.suffix = suffix;
-    build.cur    = '';
-    build.count  = 0;
-    build.str    = build.prefix;
-    build.init   = true;
-    return build;
+  return {
+    /**
+     * NOTE: altho it's the sym agument that is formally optional, one should
+     * think of this signature more like consume([count, ]sym):
+     * - if a single string argument is given, any numeric prefix is parsed out
+     *   and becomes count (default 1)
+     * - if a single numeric argument is given, that's a noop (empty symbol)
+     * - if two strings are given, any non-numeric suffix in the count string
+     *   is ignored
+     * - those last two cases are just unfortunate outcomes of javascript's
+     *   semimplicit type system, and not really intended use cases
+     *
+     * @param {string|number} count
+     * @param {string} [sym]
+     */
+    consume(count, sym) {
+      if (sym === undefined) {
+        if (typeof count != 'string') { return }
+        const match = /(\d*)(\w+)/.exec(count);
+        if (!match) { return }
+        count = match[1], sym = match[2];
+      }
+      if (typeof count == 'string') {
+        count = parseInt(count);
+        if (isNaN(count)) {
+          count = 1;
+        }
+      }
 
-    function build(mult, sym) {
-        if (build.cur !== sym) {
-            if (build.cur && build.count) {
-                if (build.init) {
-                    build.init = false;
-                } else {
-                    build.str += build.sep;
-                }
-                if (build.count > 1) {
-                    build.str += build.count.toString();
-                }
-                build.str += build.cur;
-            }
-            build.cur = sym || '';
-            build.count = 0;
+      if (cur !== sym) {
+        if (cur && curCount) {
+          parts.push([curCount, cur]);
         }
-        if (mult === 0 && !sym) {
-            var ret     = build.str + build.suffix;
-            build.cur   = '';
-            build.count = 0;
-            build.str   = build.prefix;
-            build.init  = false;
-            return ret;
-        }
-        build.count += mult;
-        return '';
+        cur = sym, curCount = 0;
+      }
+      curCount += count;
+    },
+
+    finish() {
+      if (cur && curCount) {
+        parts.push([curCount, cur]);
+      }
+      const res = parts;
+      parts = [], cur = '', curCount = 0;
+      return res;
     }
+  };
+}
+
+/** @param {Iterable<[count: string|number, sym?: string]>} countsyms */
+export function from(countsyms) {
+  const { consume, finish } = RLEBuilder();
+  for (const [count, sym] of countsyms) {
+    consume(count, sym);
+  }
+  return finish();
 }
