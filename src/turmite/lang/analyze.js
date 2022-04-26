b6c5d71d65a0334fa484f415dc5ab1f3fed6c890
Joshua T Corbin <jcorbin@wunjo.org>
2022-04-26 12:14:56 -0400

    Uplift turmite/lang/analyze.js

--- src/turmite/lang/analyze.js
+++ src/turmite/lang/analyze.js
@@ -1,81 +1,100 @@
+// @ts-check
+
 'use strict';
 
-var walk = require('./walk.js');
+import * as rezult from '../../rezult.js';
+import * as walk from './walk.js';
 
-// pre-processing step for compilation
-module.exports = analyze;
+/** @typedef {object} analysis
+ * @prop {number} maxTurns
+ */
 
-function analyze(spec, scope) {
-    walk.iter(spec, function _each(node, next) {
-        each(node, spec, scope);
-        next();
-    });
-}
+/**
+ * @param {walk.SpecNode} spec
+ * @param {Set<string>} symbols
+ * @returns {rezult.Result<analysis>}
+ */
+export function analyze(spec, symbols) {
+  let maxTurns = 0;
 
-function each(node, spec, scope) {
-    switch (node.type) {
-    case 'assign':
-        scope[node.id.name] = node.value;
-        break;
-
-    case 'member':
-        if (node.value.type !== 'symbol' &&
-            node.value.type !== 'identifier') {
-            node.value = hoist(
-                gensym(node.value.type, scope),
-                node.value,
-                spec, scope);
-        }
-        break;
-
-    case 'turns':
-        scope.numColors = Math.max(scope.numColors, node.value.length);
-        break;
-
-    case 'then':
-        if (node.turn.type === 'turns') {
-            var colorSyms = walk.collect(node.color, isSymOrId);
-            if (colorSyms.length === 1) {
-                node.turn = {
-                    type: 'member',
-                    value: node.turn,
-                    item: colorSyms[0]
-                };
-            }
-            // TODO: else error
-        }
-        break;
+  for (const { id: { name } } of spec.assigns) {
+    if (symbols.has(name)) {
+      return rezult.error(new Error(`redefinition of special symbol ${name}`));
     }
-}
+    symbols.add(name);
+  }
 
-function hoist(name, value, spec, scope) {
-    scope[name] = value;
+  /**
+   * @param {walk.AnyExpr} value
+   * @returns {walk.IdentifierNode}
+   */
+  function assign(value) {
+    const name = gensym(value.type, symbols);
+    symbols.add(name);
     spec.assigns.push({
-        type: 'assign',
-        id: {
-            type: 'identifier',
-            name: name
-        },
-        value: value
+      type: 'assign',
+      id: { type: 'identifier', name },
+      value,
     });
-    each(value, spec, scope);
-    return {
-        type: 'identifier',
-        name: name
-    };
-}
+    each(value); // since dfs will no longer visit the value
+    return { type: 'identifier', name };
+  }
 
-function gensym(kind, scope) {
-    var sym = kind[0].toUpperCase() +
-        kind.slice(1);
-    var i = 1;
-    while (scope[sym + i]) {
-        i++;
+  walk.dfsPre(spec, each);
+
+  /** @param {walk.Node} node */
+  function each(node) {
+    switch (node.type) {
+      case 'assign':
+        symbols.add(node.id.name);
+        break;
+
+      case 'member':
+        switch (node.value.type) {
+          case 'symbol':
+          case 'identifier':
+            break;
+          default:
+            node.value = assign(node.value);
+        }
+        break;
+
+      case 'turns':
+        maxTurns = Math.max(maxTurns, node.value.length);
+        break;
+
+      // TODO what was this doing? hacking the language tree to make then turns indexed by color implicitly?
+      // case 'then':
+      //   if (node.turn.type === 'turns') {
+      //     var colorSyms = walk.collect(node.color, ({ type }) =>
+      //       type === 'symbol' || type === 'identifier');
+      //     if (colorSyms.length === 1) {
+      //       node.turn = {
+      //         type: 'member',
+      //         value: node.turn,
+      //         item: colorSyms[0]
+      //       };
+      //     }
+      //     // TODO: else error
+      //   }
+      //   break;
+
     }
-    return sym + i;
+  }
+
+  return rezult.just({ maxTurns });
 }
 
-function isSymOrId(child) {
-    return child.type === 'symbol' ||
-           child.type === 'identifier';
+/**
+ * @param {string} kind
+ * @param {Set<string>} symbols
+ */
+function gensym(kind, symbols) {
+  const sym = kind[0].toUpperCase() + kind.slice(1);
+  for (let i = 1; /* TODO non-infinite? */; i++) {
+    const name = sym + i;
+    if (!symbols.has(name)) {
+      return name;
+    }
+  }
 }
