d61e3bb6bbe8042c85225a1066a64d2447c578ee
Joshua T Corbin <jcorbin@wunjo.org>
2022-05-05 16:38:45 -0400

    Uplift turmite/index.js
    Refactored step over world.updateEnt and constants.turnDirs

--- src/turmite/index.js
+++ src/turmite/index.js
@@ -1,162 +1,109 @@
+// @ts-check
+
 'use strict';
 
-var Coord = require('../coord.js');
-var CubePoint = Coord.CubePoint;
-var constants = require('./constants.js');
-var parseTurmite = require('./parse.js');
+import { OddQOffset } from '../coord.js';
+import * as rezult from '../rezult.js';
 
-module.exports = Turmite;
+import * as constants from './constants.js';
 
-/*
- * state, color -> nextState, write, turn
- *
- * index struct {
- *     state u8
- *     color u8
- * }
- *
- * rule struct {
- *     nextState u8
- *     write     u8
- *     turn      u16 // bit-field
- * }
- */
-
-Turmite.ruleHelp =
-    'ant(<number>?<turn> ...) , turns:\n' +
-    '  - L=left, R=right\n' +
-    '  - B=back, F=forward\n' +
-    '  - P=port, S=starboard (these are rear-facing left/right)\n' +
-    '\n' +
-    'See README for full turmite language details.'
-    ;
-
-function Turmite() {
-    this.numStates = 0;
-    this.numColors = 0;
-    this.rules = new Uint32Array(256 * 256);
-    this.specString = '';
-
-    this.dir = 0;
-    this.pos = CubePoint(0, 0, 0);
-
-    this.state = 0;
-
-    this.index = 0;
+export function* ruleHelp() {
+  // TODO make this interactive by taking an input argument
+  yield 'ant(<number>?<turn> ...) , turns:'
+  yield '  - L=left, R=right'
+  yield '  - B=back, F=forward'
+  yield '  - P=port, S=starboard (these are rear-facing left/right)'
+  yield ''
+  yield 'See README for full turmite language details.'
 }
 
-Turmite.prototype.reset =
-function reset() {
+export { default as parse } from './parse.js';
+
+/** @typedef {import('./lang/compile.js').RuleConstants} RuleConstants */
+import parse from './parse.js';
+
+export class Turmite {
+
+  /**
+   * @param {RuleConstants} ruleSpec
+   * @param {string} str
+   * @param {Turmite} [ent]
+   * @returns {rezult.Result<Turmite>}
+   */
+  static from(ruleSpec, str, ent = new Turmite()) {
+    const { value: build, err: parseErr } = parse(str);
+    if (parseErr) {
+      return rezult.error(parseErr);
+    }
+
+    ent.reset();
+    this.rules = new Uint32Array(256 * 256);
+
+    const { value: built, err: buildErr } = build(ent.rules, ruleSpec);
+    if (buildErr) {
+      return rezult.error(buildErr);
+    }
+
+    const { numColors, numStates, specString } = built;
+    ent.numColors = numColors;
+    ent.numStates = numStates;
+    ent.specString = specString;
+
+    return rezult.just(ent);
+  }
+
+  constructor() {
+    this.numStates = 0;
+    this.numColors = 0;
+    this.specString = '<null turmite>';
+    this.rules = new Uint32Array(256 * 256);
+    this.dir = 0;
+    this.pos = new OddQOffset(0, 0);
     this.state = 0;
-};
+    this.index = 0;
+  }
 
-Turmite.prototype.clearRules =
-function clearRules() {
-    for (var i = 0; i < this.rules.length; i++) {
-        this.rules[i] = 0;
-    }
-};
+  reset() {
+    this.dir = 0;
+    this.pos.scale(0);
+    this.state = 0;
+  }
 
-Turmite.parse =
-function parse(str) {
-    return parseTurmite(str);
-};
+  toString() {
+    return this.specString || '<UNKNOWN turmite>';
+  }
 
-Turmite.compile =
-function compile(str, ent) {
-    var res = Turmite.parse(str);
-    if (res.err) {
-        return res;
-    }
-    var func = res.value;
-    return func(ent || new Turmite());
-};
+  /** @typedef {object} World
+   * @prop {(index: number, fn: (dir: number, state: number, datum: number) => {dir: number, state: number, datum: number}) => void} updateEnt
+   */
 
-Turmite.prototype.toString =
-function toString() {
-    if (this.specString) {
-        return this.specString;
-    }
-    return '<UNKNOWN turmite>';
-};
+  /** @param {World} world */
+  step(world) {
+    const { rules, index } = this;
+    world.updateEnt(index, (dir, state, datum) => {
 
-Turmite.prototype.step =
-function step(world) {
-    var self = this;
-    var tile = world.tile;
-    var pos = world.getEntPos(this.index);
-    var turn = 0;
+      // TODO use constants rather than these hardcodes
+      const color = datum & 0x00ff;
+      const flags = datum & 0xff00;
+      const ruleIndex = state << 8 | color;
+      const rule = rules[ruleIndex];
+      const turn = rule & 0x0000ffff;
+      const write = (rule & 0x00ff0000) >> 16;
+      state = (rule & 0xff000000) >> 24;
+      datum = flags | write | 0x0100;
 
-    tile.update(pos, update);
-    world.turnEnt(this.index, executeTurn);
-    if (turn !== 0) {
-        throw new Error('turmite forking unimplemented');
-    }
+      const newDirs = [...constants.turnDirs(turn, dir)];
+      switch (newDirs.length) {
+        case 0:
+          break;
+        case 1:
+          dir = newDirs[0];
+          break;
+        default:
+          throw new Error('turmite forking unimplemented');
+      }
 
-    // TODO: WIP
-    // var self = null;
-    // while (turn !== 0) {
-    //     if (self) {
-    //         self = self.fork();
-    //     } else {
-    //         self = this;
-    //     }
-    //     world.tuneEnt(self.index, executeTurn);
-    // }
-
-    function update(data) {
-        var color = data & 0x00ff;
-        var flags = data & 0xff00;
-        var ruleIndex = self.state << 8 | color;
-        var rule = self.rules[ruleIndex];
-        turn = rule & 0x0000ffff;
-        var write = (rule & 0x00ff0000) >> 16;
-        self.state = (rule & 0xff000000) >> 24;
-        return flags | write | 0x0100;
-    }
-
-    function executeTurn(dir) {
-        var t = 1;
-        for (; t <= 0x0020; t <<= 1) {
-            if (turn & t) {
-                turn &= ~t;
-                return (6 + dir + constants.RelTurnDelta[t]) % 6;
-            }
-        }
-        for (; t <= 0x0800; t <<= 1) {
-            if (turn & t) {
-                turn &= ~t;
-                return constants.AbsTurnDir[t];
-            }
-        }
-        if (turn !== 0) {
-            throw new Error('unrecognized turning constant ' + turn);
-        }
-        return dir;
-    }
-};
-
-// TODO: WIP
-// Turmite.prototype.fork =
-// function fork() {
-//     // TODO: ability to steal an already allocated ant from world pool
-//     var self = new Turmite(this.world, this.rules);
-
-//     // self.world = this.world;
-//     // self.rules = this.rules;
-
-//     self.numStates = this.numStates;
-//     self.numColors = this.numColors;
-//     self.specString = this.specString;
-//     self.dir = this.oldDir;
-//     self.oldDir = this.oldDir;
-//     self.pos.copyFrom(this.oldPos);
-//     self.oldPos.copyFrom(this.oldPos);
-//     self.state = this.state;
-//     self.size = this.size;
-//     self.index = this.index;
-
-//     // TODO: add to world
-
-//     return self;
-// };
+      return { dir, state, datum };
+    });
+  }
+}
