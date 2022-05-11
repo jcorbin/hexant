aa2a414b4e18536411be60304251ed876f8a6ec3
Joshua T Corbin <jcorbin@wunjo.org>
2022-05-11 09:02:01 -0400

    Add a terser-minified bundle

--- rollup.config.js
+++ rollup.config.js
@@ -4,14 +4,22 @@
 
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 
 /** @type {import('rollup').RollupOptions} */
 const config = {
   input: 'src/hexant.js',
-  output: {
-    file: 'public/hexant.js',
-    format: 'esm',
-  },
+  output: [
+    {
+      file: 'public/hexant.js',
+      format: 'esm',
+    },
+    {
+      file: 'public/hexant.min.js',
+      format: 'esm',
+      plugins: [terser()],
+    },
+  ],
   plugins: [nodeResolve(), commonjs()],
 };
 
