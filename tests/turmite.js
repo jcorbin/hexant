17e245ccf061e6601d54e5b3b1e554f2de082eb7
Joshua T Corbin <jcorbin@wunjo.org>
2022-05-19 11:25:50 -0400

    tests/turmite: refactor test suite utilitites
    - generate expected rules lazily using imap(genn(N))
      - note imap passes index for later use as key index
    - rename hexem() in line with hexkey()
    - improve hexrule presentation:
      - let state and color be normal numbers since they aren't bit fields.
    - fix/improve hexkey presentation
      - fix incorrect arg order to hexit()
      - only hex the key itself, which has state and color bit fields
      - but let state and color present as normal numbers, since they aren't
        bit fields
    - factor out {can, is}Turmite()
    - simplify decodeKey() usage

--- tests/turmite.js
+++ tests/turmite.js
@@ -6,7 +6,7 @@ import { Turn } from '../src/turmite/constants.js';
 import { Turmite, compile } from '../src/turmite/index.js';
 
 // test basic ants
-for (const { name, input, expected: { numColors, numStates, rules } } of [
+for (const { name, input, expected } of [
 
   {
     name: 'basic LR',
@@ -14,44 +14,20 @@ for (const { name, input, expected: { numColors, numStates, rules } } of [
     expected: {
       numColors: 2,
       numStates: 1,
-      rules: [
-        // [0, Turn.RelLeft, 1],
-        // [1, Turn.RelRight, 2],
+      rules: imap(genn(256), /** @returns {RuleTuple} */ color => [
+        // [0, Turn.RelLeft, 1]
+        // [1, Turn.RelRight, 2]
         // ...
-        // [254, Turn.RelLeft, 255],
-        // [255, Turn.RelRight, 0],
-        ...genn(256)].map(i => [
-          i,
-          i % 2 ? Turn.RelRight : Turn.RelLeft,
-          (i + 1) % 256
-        ]),
+        // [254, Turn.RelLeft, 255]
+        // [255, Turn.RelRight, 0]
+        0, color,
+        0, (color + 1) % 256,
+        color % 2 ? Turn.RelRight : Turn.RelLeft,
+      ]),
     },
   },
 
-]) test(name, t => {
-  const { err, value: ent } = Turmite.from(input);
-  if (t.falsy(err)) {
-    t.is(ent.toString(), input);
-    t.is(ent.numColors, numColors);
-    t.is(ent.numStates, numStates);
-    for (const [key, turn, color] of rules) {
-      const state = 0; // ants are stateless
-      const rule = ent.rules[key];
-      const ruleTurn = rule & 0xffff;
-      const ruleColor = (rule >> 16) & 0xff;
-      const ruleState = (rule >> 24) & 0xff;
-      t.is(
-        hexit(4, ruleTurn), hexit(4, turn),
-        `rules[${hexit(key, 4)}] -> turn`);
-      t.is(
-        hexit(2, ruleColor), hexit(2, color),
-        `rules[${hexit(key, 4)}] -> color`);
-      t.is(
-        hexit(2, ruleState), hexit(2, state),
-        `rules[${hexit(key, 4)}] -> state`);
-    }
-  }
-});
+]) test(name, t => canTurmite(t, input, expected));
 
 // test basic turmites vs equivalent ants
 for (const { name, input, equiv } of [
@@ -72,31 +48,80 @@ for (const { name, input, equiv } of [
   },
 
 ]) test(`equivalant: ${name} `, t => {
-  const inStr = Array.isArray(input) ? input.join('\n') : input;
-  const { err: entErr, value: ent } = Turmite.from(inStr);
-  const { err: eqvErr, value: eqv } = Turmite.from(equiv);
-  if (t.falsy(entErr) && t.falsy(eqvErr)) {
-    t.is(ent.toString().trim(), inStr);
-    t.is(ent.numColors, eqv.numColors, 'numColors');
-    t.is(ent.numStates, eqv.numStates, 'numStates');
-    for (let key = 0; key < ent.rules.length; key++) t.deepEqual(
-      hexem(decodeRule(ent.rules[key])),
-      hexem(decodeRule(eqv.rules[key])),
-      `rules[${hexkey(key)}]`);
-  }
+  const { err, value } = Turmite.from(equiv);
+  if (!t.falsy(err)) return;
+  canTurmite(t, input, equivProps(value));
+});
 
+/** @typedef {string|string[]} TestInput */
+
+/** @typedef {[keyState: number, keyColor: number, state: number, color: number, turn: number]} RuleTuple */
+
+/** @typedef {object} ExpectProps
+ * @prop {number} numColors
+ * @prop {number} numStates
+ * @prop {Iterable<RuleTuple>} rules
+ */
+
+/**
+ * @param {Turmite} ent
+ * @returns {ExpectProps}
+ */
+function equivProps(ent) {
+  const { numColors, numStates, rules } = ent;
+  return {
+    numColors,
+    numStates,
+    rules: imap(rules, /** @returns {RuleTuple} */(rule, key) => {
+      const { keyState, keyColor } = decodeKey(key);
+      const { state, color, turn } = decodeRule(rule);
+      return [keyState, keyColor, state, color, turn];
+    }),
+  };
+}
+
+/**
+ * @param {import('ava').ExecutionContext<unknown>} t
+ * @param {TestInput} input
+ * @param {Partial<ExpectProps>} expected
+ */
+function canTurmite(t, input, expected) {
+  const spec = Array.isArray(input) ? input.join('\n') : input;
+  const { err, value } = Turmite.from(spec);
+  if (t.falsy(err)) isTurmite(t, value, { input, ...expected });
   if (!t.passed) {
-    for (const line of compile(inStr, { format: 'module' })) {
+    for (const line of compile(spec, { format: 'module' })) {
       t.log(line);
     }
   }
+}
 
-});
+/**
+ * @param {import('ava').ExecutionContext<unknown>} t
+ * @param {Turmite} ent
+ * @param {Partial<{input: TestInput} & ExpectProps>} expected
+ */
+function isTurmite(t, ent, { input, numColors, numStates, rules }) {
+  if (input !== undefined) t.is(
+    ent.toString().trim(),
+    (Array.isArray(input) ? input.join('\n') : input).trim(),
+    'input vs specString (canonical)');
+  if (numColors !== undefined) t.is(ent.numColors, numColors, 'numColors');
+  if (numStates !== undefined) t.is(ent.numStates, numStates, 'numStates');
+  for (const [keyState, keyColor, state, color, turn] of rules) {
+    const key = keyState << 8 | keyColor;
+    t.deepEqual(
+      hexrule(decodeRule(ent.rules[key])),
+      hexrule({ state, color, turn }),
+      `rules[${hexkey(key)}]`);
+  }
+}
+
 
 /** @param {number} key */
 function hexkey(key) {
-  const { color: keyColor, state: keyState } = decodeKey(key);
-  return `state:${hexit(keyState, 2)} color:${hexit(keyColor, 2)}`
+  const { keyColor, keyState } = decodeKey(key);
+  return `${hexit(4, key)} state:${keyState} color:${keyColor}`
 }
 
 /** @param {object} tcs
@@ -104,20 +129,16 @@ function hexkey(key) {
  * @param {number} tcs.color
  * @param {number} tcs.state
  */
-function hexem({ turn, color, state }) {
-  return {
-    turn: hexit(4, turn),
-    color: hexit(2, color),
-    state: hexit(2, state),
-  };
+function hexrule({ turn, color, state }) {
+  return { state, color, turn: hexit(4, turn) };
 
 }
 
 /** @param {number} key */
 function decodeKey(key) {
-  const color = key & 0xff;
-  const state = (key >> 8) & 0xff;
-  return { color, state };
+  const keyColor = key & 0xff;
+  const keyState = (key >> 8) & 0xff;
+  return { keyColor, keyState };
 }
 
 /** @param {number} rule */
@@ -145,4 +166,15 @@ function* genn(n, start = 0) {
  */
 function hexit(n, val) {
   return `0x${val.toString(16).padStart(n, '0')}`
+}
+
+/** @template T, V
+ * @param {Iterable<T>} it
+ * @param {(t: T, i: number) => V} fn
+ */
+function* imap(it, fn) {
+  let i = 0;
+  for (const item of it) {
+    yield fn(item, i++);
+  }
 }
