jcorbin/hexant

commit 7885fe5ba4be47aa6141658299c8e2d343b01d0f

HexTileTreeNode#expand: subsume tile.grow

hextiletree.js
@@ -162,7 +162,12 @@ function expand() {
     for (var i = 0; i < this.tiles.length; i++) {
         var tile = this.tiles[i];
         if (tile !== null) {
-            node.tiles[i] = tile.grow(i);
+            var tileNode = new HexTileTreeNode(tile.growthOrigin(i), node.tileSize);
+            tileNode.tiles[zoomPerm[i]] = tile;
+            if (tile.data !== undefined) {
+                tileNode.concrete++;
+            }
+            node.tiles[i] = tileNode;
         }
     }
     return node;
@@ -180,25 +185,6 @@ function growthOrigin(i) {
     return this.origin.copy().add(
         this.oqo.copyFrom(nodeOriginOffset[i]).scale(this.tileSize)
     );
-};
-
-OddQHexTile.prototype.grow =
-function grow(i) {
-    var offset = tileOriginOffset[i].copy().scale(this.width);
-    var origin = this.origin.copy().add(offset);
-    var node = new HexTileTreeNode(origin, 2 * this.width);
-    node.tiles[zoomPerm[i]] = this;
-    node.concrete++;
-    return node;
-};
-
-HexTileTreeNode.prototype.grow =
-function grow(i) {
-    var offset = nodeOriginOffset[i].copy().scale(this.tileSize);
-    var origin = this.origin.copy().add(offset);
-    var node = new HexTileTreeNode(origin, 2 * this.size);
-    node.tiles[zoomPerm[i]] = this;
-    return node;
 };
 
 HexTileTreeNode.prototype.boundingBox =

commit d3943495525c369c72a38b24625751c13352ec9f

Uplift {src/test => tests}/hextiletree.js
Was actually broken since 7885fe5 removed OddQHexTile.prototype.grow 6
years ago.

src/test/hextiletree.js
@@ -1,98 +1,0 @@
-'use strict';
-
-var test = require('tape');
-
-var OddQOffset = require('../coord.js').OddQOffset;
-var OddQHexTile = require('../hextile.js');
-// var HexTileTree = require('../hextiletree.js');
-require('../hextiletree.js');
-
-[
-    OddQOffset(-1, -1),
-    OddQOffset(0, -1),
-    OddQOffset(-1, 0),
-    OddQOffset(0, 0)
-].forEach(function each(origin, i) {
-    var plan = [
-        ['initTile', origin, 1, 1],
-        ['check', [[0]]],
-        ['set', origin, 1],
-        ['check', [[1]]]
-    ];
-    for (var n = 1; n <= 3; n++) {
-        var N = Math.pow(2, n);
-        var expected = zeros(N);
-        var row = expected[(N + origin.r) % N];
-        row[(N + origin.q) % N] = 1;
-        plan.push(
-            ['grow', i],
-            ['check', expected]);
-    }
-    testPlan('grow(' + i + ')', plan);
-
-});
-
-var Actions = {};
-
-Actions.initTile = function initTile(origin, width, height) {
-    this.tile = new OddQHexTile(origin, width, height);
-};
-
-Actions.set = function set(point, value) {
-    this.tile.set(point, value);
-};
-
-Actions.grow = function grow(i) {
-    this.tile = this.tile.grow(i);
-};
-
-Actions.check = function check(expected) {
-    var topLeft = this.tile.boundingBox().topLeft;
-    var out = [];
-    this.tile.eachDataPoint(function each(point, d) {
-        var q = point.q - topLeft.q;
-        var r = point.r - topLeft.r;
-        var row = out[r] || (out[r] = []);
-        row[q] = d;
-    });
-
-    this.assert.deepEqual(out, expected, 'expected ' + disp(expected));
-};
-
-function testPlan(desc, plan) {
-    test(desc, function t(assert) {
-        runTestPlan(assert, plan);
-        assert.end();
-    });
-}
-
-function runTestPlan(assert, plan) {
-    var ctx = {
-        assert: assert,
-        tile: null
-    };
-    plan.forEach(function eachStep(inst) {
-        // assert.comment(disp(inst));
-        Actions[inst[0]].apply(ctx, inst.slice(1));
-        // console.log(ctx.tile.dump());
-    });
-}
-
-function disp(arg) {
-    if (Array.isArray(arg)) {
-        return '[' + arg.map(disp).join(' ') + ']';
-    }
-
-    return arg.toString();
-}
-
-function zeros(N) {
-    var ar = [];
-    for (var i = 0; i < N; i++) {
-        ar[i] = [];
-        for (var j = 0; j < N; j++) {
-            ar[i][j] = 0;
-        }
-    }
-    return ar;
-}

tests/hextiletree.js
@@ -1,0 +1,123 @@
+// @ts-check
+
+import test from 'ava';
+
+import { OddQOffset } from '../src/coord.js';
+import { OddQHexTile } from '../src/hextile.js';
+import { growTile } from '../src/hextiletree.js';
+
+/** @typedef {import('../src/hextile.js').oddQPotent} oddQPotent */
+/** @typedef {import('../src/hextiletree.js').HexTileTreeNode} HexTileTreeNode */
+
+[
+  new OddQOffset(-1, -1),
+  new OddQOffset(0, -1),
+  new OddQOffset(-1, 0),
+  new OddQOffset(0, 0)
+].forEach((origin, i) => {
+
+  const plan = [
+    ['initTile', origin, 1, 1],
+    ['check', [[0]]],
+    ['set', origin, 1],
+    ['check', [[1]]]
+  ];
+
+  for (let n = 1; n <= 3; n++) {
+    const N = Math.pow(2, n);
+
+    const expected = zeros(N);
+    const row = expected[(N + origin.r) % N];
+    row[(N + origin.q) % N] = 1;
+
+    plan.push(
+      ['grow', i],
+      ['check', expected]);
+  }
+
+  test(`grow(${i})`, t => {
+    const ctx = { t, tile: null };
+    for (const step of plan) {
+      // assert.comment(disp(step));
+      Actions[step[0]].apply(ctx, step.slice(1));
+      // console.log(ctx.tile.dump());
+    }
+  });
+
+});
+
+/** @typedef {object} Context
+ * @prop {import('ava').ExecutionContext} t
+ * @prop {null|OddQHexTile|HexTileTreeNode} tile
+ */
+
+const Actions = {
+
+  /**
+   * @param {oddQPotent} origin
+   * @param {number} width
+   * @param {number} height
+   * @this {Context}
+   */
+  initTile(origin, width, height) {
+    this.tile = new OddQHexTile();
+    this.tile.init(origin, width, height);
+  },
+
+  /**
+   * @this {Context}
+   * @param {oddQPotent} point
+   * @param {number} value
+   */
+  set(point, value) {
+    this.tile.set(point, value);
+  },
+
+  /**
+   * @param {number} i
+   * @this {Context}
+   */
+  grow(i) {
+    this.tile = growTile(null, this.tile, i);
+  },
+
+  /**
+   * @param {number[][]} expected
+   * @this {Context}
+   */
+  check(expected) {
+    const topLeft = this.tile.boundingBox().topLeft;
+    /** @type {number[][]} */
+    const out = [];
+    this.tile.eachDataPoint((point, d) => {
+      const q = point.q - topLeft.q;
+      const r = point.r - topLeft.r;
+      const row = out[r] || (out[r] = []);
+      row[q] = d;
+    }, 0);
+
+    this.t.deepEqual(out, expected, `expected ${disp(expected)}`);
+  },
+
+};
+
+/** @param {any} arg */
+function disp(arg) {
+  if (Array.isArray(arg)) {
+    return `[${arg.map(disp).join(' ')}]`;
+  }
+  return arg.toString();
+}
+
+/** @param {number} N */
+function zeros(N) {
+  /** @type {number[][]} */
+  const ar = [];
+  for (let i = 0; i < N; i++) {
+    ar[i] = [];
+    for (let j = 0; j < N; j++) {
+      ar[i][j] = 0;
+    }
+  }
+  return ar;
+}